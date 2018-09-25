b05378b474d49bc3cf5653c9315b7e3490763dda
0x00red <43559748+0x00red@users.noreply.github.com>
2018-09-25 11:15:29 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -6,28 +6,38 @@ int main(int argc, char *argv[]) {
         printf("Usage: eXOR input_file key_file\n");
         return 0;
     }
+
+
     //above line checks for the correct arguments, exiting otherwise;
     FILE *iFile; //input file pointer;
     FILE *kFile; //key file pointer;
-    iFile = fopen(argv[1], "r"); // opens input file for read;
-    kFile = fopen(argv[2], "r"); // opens key file for read;
+    iFile = fopen(argv[1], "rb"); // opens input file for binary read;
+    kFile = fopen(argv[2], "rb"); // opens key file for binary read;
     int iLen = getFileLen(iFile); //get file length;
     int kLen = getFileLen(kFile); //get file length;
-    iLen++; //to accomodate for EOF character;
+    int maxLen = iLen * kLen; // guarantee we have enough space;
+    maxLen++; //to accomodate for EOF character;
+
+
     //now we set and load character buffers for both;
-    char iBuffer[iLen];
-    char kBuffer[iLen];
+    char iBuffer[maxLen];
+    char kBuffer[maxLen];
+
+
     //loading character buffer;
-    fgets(iBuffer, iLen-1, iFile); //input file should be larger than key file;
-    fgets(kBuffer, iLen-1, kFile);
-    iBuffer[iLen - 1] = '\0'; //terminating characters;
-    kBuffer[iLen - 1] = '\0';
+    fgets(iBuffer, maxLen-1, iFile); //input file should be larger than key file;
+    fgets(kBuffer, maxLen-1, kFile);
+    iBuffer[maxLen - 1] = '\0'; //terminating characters;
+    kBuffer[maxLen - 1] = '\0';
+
+
     int i = 0; //incrementor for loop;
-    while(i<iLen-1) {
+    while(i<maxLen-1) {
         iBuffer[i] ^= kBuffer[i]; //XORs the current characters;
         i++; //increments;
     }
-    iBuffer[i] = '\0'; //EOF character, assures no data was overwritten;
+
+
     printf("%s\n", iBuffer);
     return 0;
 }
